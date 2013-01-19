645e9407e3cf61475ace6fd56bf32134a4664a23
Matt Blair <elsewisemedia@gmail.com>
2013-01-19 14:44:15 -0800

    Basic table implementation in Home VC

--- knowYourCity/Classes/HomeViewController.m
+++ knowYourCity/Classes/HomeViewController.m
@@ -7,26 +7,50 @@
 //
 
 #import "HomeViewController.h"
+#import "ThemeViewController.h"
+#import "TidbitListViewController.h"
 
 @interface HomeViewController ()
+
+@property (strong, nonatomic) NSArray *themeList;
+
+@property (strong, nonatomic) UITableView *themeTableView;
 
 @end
 
 @implementation HomeViewController
 
+#pragma mark - View Controller Lifecycle
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
+        // this array is temporary, until Core Data implemented and populated
+        self.themeList = @[@"a", @"b", @"c", @"d", @"e", @"f", @"g", @"h"];
+        self.title = NSLocalizedString(@"Know Your City", @"App title");
     }
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
+- (void)loadView {
+    
+    [super loadView];
+    
+    self.view.backgroundColor = [UIColor yellowColor];
+    
+    self.themeTableView = [[UITableView alloc] initWithFrame:[self.view bounds]
+                                                       style:UITableViewStylePlain];
+    
+    self.themeTableView.dataSource = self;
+    self.themeTableView.delegate = self;
+    
+    // turn off section index, if you use sections
+    //self.themeTableView.sectionIndexMinimumDisplayRowCount = 0;
+    
+    [self.view addSubview:self.themeTableView];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +58,51 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UITableViewDataSource 
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.themeList count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure the cell...
+    
+    if ([self.themeList count] > indexPath.row) {
+        cell.textLabel.text = self.themeList[indexPath.row];
+    } else {
+        cell.textLabel.text = @"Not Found.";
+        DLog(@"Couldn't convert section %d and row %d into a theme.", indexPath.section, indexPath.row);
+    }
+    
+    cell.textLabel.accessibilityLabel = cell.textLabel.text;
+    cell.textLabel.accessibilityHint = NSLocalizedString(@"The name of a theme.", @"Hint for theme name in theme list.");
+    
+    cell.textLabel.font = [UIFont fontWithName:kBodyFontName size:17.0];
+    cell.textLabel.textColor = [UIColor blackColor];
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleGray;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    DLog(@"Tapped section %d and row %d", indexPath.section, indexPath.row);
+}
+
 
 @end
