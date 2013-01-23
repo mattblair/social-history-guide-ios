c1a10c74852117fd6ee24fb3ba55abd910c77866
Matt Blair <elsewisemedia@gmail.com>
2013-01-23 06:55:19 -0800

    Added UIView subclasses for Story and Guest stubs

---
+++ knowYourCity/Classes/StoryStubView.h
@@ -0,0 +1,13 @@
+//
+//  StoryStubView.h
+//  knowYourCity
+//
+//  Created by Matt Blair on 1/23/13.
+//  Copyright (c) 2013 Elsewise LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryStubView : UIView
+
+@end
---
+++ knowYourCity/Classes/StoryStubView.m
@@ -0,0 +1,31 @@
+//
+//  StoryStubView.m
+//  knowYourCity
+//
+//  Created by Matt Blair on 1/23/13.
+//  Copyright (c) 2013 Elsewise LLC. All rights reserved.
+//
+
+#import "StoryStubView.h"
+
+@implementation StoryStubView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
