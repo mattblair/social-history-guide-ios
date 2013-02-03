6797a37856dd312e005dbb7a5146e2960a675aa3
Matt Blair <elsewisemedia@gmail.com>
2013-02-03 08:49:07 -0800

    Added configuration provider

---
+++ knowYourCity/Classes/KYCConfigurationProvider.h
@@ -0,0 +1,15 @@
+//
+//  KYCConfigurationProvider.h
+//  knowYourCity
+//
+//  Created by Matt Blair on 2/3/13.
+//  Copyright (c) 2013 Elsewise LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// this class has utility methods for layout, fonts and data that don't fit elsewhere
+
+@interface KYCConfigurationProvider : NSObject
+
+@end
---
+++ knowYourCity/Classes/KYCConfigurationProvider.m
@@ -0,0 +1,13 @@
+//
+//  KYCConfigurationProvider.m
+//  knowYourCity
+//
+//  Created by Matt Blair on 2/3/13.
+//  Copyright (c) 2013 Elsewise LLC. All rights reserved.
+//
+
+#import "KYCConfigurationProvider.h"
+
+@implementation KYCConfigurationProvider
+
+@end
