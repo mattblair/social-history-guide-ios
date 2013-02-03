7a558f88c9f75623e96b8ce18e184f72b3ed04a6
Matt Blair <elsewisemedia@gmail.com>
2013-02-02 19:13:20 -0800

    Clarified an implementation comment

--- knowYourCity/Classes/GuestStubView.h
+++ knowYourCity/Classes/GuestStubView.h
@@ -8,12 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-// may expand to show guest detail as modaly overlay instead of pushing a vc on the nav stack
+// may expand to show guest detail as modal overlay instead of pushing a vc on the nav stack
 // depends on how much information we have for each guest
+// If this view shows all details, it should be initialized with a Guest NSManagedObject
+// Else: implement a delegate, and tell the presenting VC to push a Guest Detail VC
 
 @interface GuestStubView : UIView
 
-// will use a Guest object as an initializer once Core Data settles
 - (id)initWithDictionary:(NSDictionary *)guestDictionary atOrigin:(CGPoint)origin;
 
 @end
