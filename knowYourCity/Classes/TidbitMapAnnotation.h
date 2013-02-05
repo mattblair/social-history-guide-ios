999c35eab99db736eb21d5b19e535868a2600cfb
Matt Blair <elsewisemedia@gmail.com>
2013-02-04 22:18:25 -0800

    Fleshed out the tidbit map annotation

--- knowYourCity/Classes/TidbitMapAnnotation.h
+++ knowYourCity/Classes/TidbitMapAnnotation.h
@@ -7,7 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
 
-@interface TidbitMapAnnotation : NSObject
+@interface TidbitMapAnnotation : NSObject <MKAnnotation>
+
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
+@property (strong, nonatomic) NSNumber *latitude;
+@property (strong, nonatomic) NSNumber *longitude;
+
+- (id)initWithLocation:(CLLocationCoordinate2D)coord;
+- (BOOL)hasPhoto;
+
+// accepts a dictionary from JSON with geometry key that has a point in it
+- (BOOL)setCoordinateFromGeoJSONFragment:(NSDictionary *)geoJSON;
 
 @end
--- knowYourCity/Classes/TidbitMapAnnotation.m
+++ knowYourCity/Classes/TidbitMapAnnotation.m
@@ -10,4 +10,57 @@
 
 @implementation TidbitMapAnnotation
 
+@synthesize coordinate=_coordinate;
+
+- (id)initWithLocation:(CLLocationCoordinate2D)coord {
+    self = [super init];
+    if (self) {
+        
+        _coordinate = coord;
+    }
+    return self;
+}
+
+- (CLLocationCoordinate2D)coordinate {
+    
+    // this assumes values have been set after init, and have been validated...
+    // use the JSON setter instead? or at least validate the values.
+    //_coordinate = CLLocationCoordinate2DMake([self.latitude doubleValue], [self.longitude doubleValue]);
+    return _coordinate;
+}
+
+- (BOOL)hasPhoto {
+    
+    //return self.photoURL && [self.photoURL length] > 0;
+    return NO;
+}
+
+- (BOOL)setCoordinateFromGeoJSONFragment:(NSDictionary *)geoJSON {
+    
+    NSArray *coordArray = [[geoJSON objectForKey:@"geometry"] objectForKey:@"coordinates"];
+    
+    BOOL coordinateValid = YES;
+    
+    CLLocationDegrees latitudeValue = [coordArray[1] doubleValue];
+    CLLocationDegrees longitudeValue = [coordArray[0] doubleValue];
+    
+    if (latitudeValue > 90.0 || latitudeValue < -90.0 ) {
+        coordinateValid = NO;
+    }
+    
+    if (longitudeValue > 180.0 || latitudeValue < -180.0 ) {
+        coordinateValid = NO;
+    }
+    
+    if (coordinateValid) {
+        _coordinate = CLLocationCoordinate2DMake(latitudeValue, longitudeValue);
+    } else {
+        
+        // defaulting to Portlandish
+        _coordinate = CLLocationCoordinate2DMake(45.5, -122.6);
+    }
+    
+    return coordinateValid;
+}
+
 @end
