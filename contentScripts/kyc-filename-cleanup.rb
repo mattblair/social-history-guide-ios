b0d9ac94d7ca5d9680a61c2197ffa3ca9a25a4d0
Matt Blair <elsewisemedia@gmail.com>
2013-05-30 07:37:52 -0700

    Added more punctuation to filename cleanup

--- contentScripts/kyc-filename-cleanup.rb
+++ contentScripts/kyc-filename-cleanup.rb
@@ -12,7 +12,7 @@ def clean_audio_filename (raw_filename)
   else
     # strip extension -- could be mp3, aif or wav -- or ?
     name = raw_filename.gsub(/(.aif|.wav|.mp3)/,"")
-    return name.gsub(/(\(|\)| )/, "-").gsub("'", "").downcase
+    return name.gsub(/(\(|\)| |\.|,)/, "-").gsub("'", "").downcase
   end
   
 end
