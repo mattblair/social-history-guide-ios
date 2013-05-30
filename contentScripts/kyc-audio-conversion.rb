94dfdbb28834fcb48c78fdd03fd9c8acb476073e
Matt Blair <elsewisemedia@gmail.com>
2013-05-30 07:37:37 -0700

    Added mp3 generation to audio script

--- contentScripts/kyc-audio-conversion.rb
+++ contentScripts/kyc-audio-conversion.rb
@@ -6,6 +6,7 @@ media_source_directory = "/Users/matt/Dropbox/kycInterviewsOriginal"
 
 ios_output_directory = "/Users/matt/Documents/codeProjects/knowYourCity/knowYourCity/Resources/audio"
 web_output_directory = "/Users/matt/Dropbox/appWorkingNotes/knowYourCity/mp4"
+preview_output_directory = "/Users/matt/Dropbox/appWorkingNotes/knowYourCity/mp3"
 
 ios_suffix = "caf"
 web_suffix = "mp4"
@@ -13,18 +14,20 @@ web_suffix = "mp4"
 # themes to process:
 
 themes_to_process = [
+=begin
   { "name" => "African-American History", "prefix" => "aam", "subdirectory" => "africanAmericanHistory" },
   { "name" => "Chinese History", "prefix" => "chi", "subdirectory" => "chineseHistory" },
   # need to get original wav or aif for these...
-  #{ "name" => "Skid Row", "prefix" => "skr", "subdirectory" => "skidRow" },
-  #{ "name" => "Women's History", "prefix" => "wmn", "subdirectory" => "womensHistory" },
+  { "name" => "Skid Row", "prefix" => "skr", "subdirectory" => "skidRow" },
+  { "name" => "Women's History", "prefix" => "wmn", "subdirectory" => "womensHistory" },
   { "name" => "Jewish History", "prefix" => "jew", "subdirectory" => "jewishHistory" },
-  { "name" => "Gay History", "prefix" => "gay", "subdirectory" => "gayHistory" }
-  #{ "name" => "Japanese-American History", "prefix" => "jpn", "subdirectory" => "japaneseAmericanHistory" },
-  #{ "name" => "Lesbian History", "prefix" => "lsb", "subdirectory" => "lesbianHistory" },
+  { "name" => "Gay History", "prefix" => "gay", "subdirectory" => "gayHistory" },
+=end
+  { "name" => "Japanese-American History", "prefix" => "jpn", "subdirectory" => "japaneseAmericanHistory" },
+  { "name" => "Lesbian History", "prefix" => "lsb", "subdirectory" => "lesbianHistory" }
   #{ "name" => "Native History", "prefix" => "nat", "subdirectory" => "nativeHistory" }
-  #{ "name" => "Native History", "prefix" => "cb", "subdirectory" => "caryeBye" }
-  #{ "name" => "Native History", "prefix" => "prl", "subdirectory" => "pearlDistrictArtists" }
+  #{ "name" => "Public Art", "prefix" => "cb", "subdirectory" => "caryeBye" }
+  #{ "name" => "Pearl District", "prefix" => "prl", "subdirectory" => "pearlDistrictArtists" }
   ]
   
 
@@ -41,13 +44,14 @@ themes_to_process.each do |theme|
   Dir.chdir("#{media_source_directory}/#{theme['subdirectory']}")
   
   # for now, loop through all the files in the dir
-  Dir.glob("*.{wav,aif}") do |filename|
+  Dir.glob("*.{wav,aif,mp3}") do |filename|
   
     # clean up the name, prepend the prefix and add an id number?
     new_file_name = "#{theme['prefix']}-#{clean_audio_filename(filename)}"
   
     # inject theme-specific prefix, because original filenames are inconsistent
-    output_file = "#{web_output_directory}/#{new_file_name}.#{web_suffix}"
+    preview_output_file = "#{preview_output_directory}/#{new_file_name}.mp3"
+    web_output_file = "#{web_output_directory}/#{new_file_name}.#{web_suffix}"
     ios_output_file = "#{ios_output_directory}/#{new_file_name}.#{ios_suffix}"
   
     #puts "Will write to #{output_file}..."
@@ -60,11 +64,16 @@ themes_to_process.each do |theme|
   
     # mp3 conversion not supported without a 3rd party encoder
     # `afconvert -v -c 1 -f MPG3 -d mp3 -s 3 -b 128000 #{filename} #{output_file}`
+    
+    # Get LAME encoder via homebrew: brew install lame
+    # lame --help
+    # http://lame.cvs.sourceforge.net/viewvc/lame/lame/USAGE
+    `lame -h \"#{filename}\" #{preview_output_file}`
   
-    # mp4:
-    # `afconvert -v -c 1 -f mp4f -d aac -s 3 -b 128000 \"#{filename}\" #{output_file}`
+    # mp4 for the web:
+    `afconvert -v -c 1 -f mp4f -d aac -s 3 -b 128000 \"#{filename}\" #{web_output_file}`
   
-    # convert the file for the app
+    # convert the file for the app:
     # `afconvert -v -c 1 -f caff -d aac -s 3 -b 128000 \"#{filename}\" #{ios_output_file}`
   
     # add an object to the output list, with id, original name, new filenames?
