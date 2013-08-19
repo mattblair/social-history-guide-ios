b8bd08ed6c33020d8bf8969bfa761f3c3cf9f101
Matt Blair <elsewisemedia@gmail.com>
2013-08-19 14:19:06 -0700

    Cleaned up comments

--- knowYourCity/Classes/KYCPrivateConstants.h
+++ knowYourCity/Classes/KYCPrivateConstants.h
@@ -8,10 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-// TestFlight config
-
-extern NSString * const kTestFlightTeamToken; // deprecated as of 1.2
-
 // required for TestFlight SDK 1.2+
 extern NSString* const kTestFlightAppToken;
 
@@ -23,8 +19,8 @@ extern NSString* const kFlurryAPIKey;
 
 // Add a KYCPrivateConstants.m files that looks like:
 
-// NSString* const kTestFlightAppToken = @"appToken";
 // Or comment out all reference to TestFlight if not using it.
+// NSString* const kTestFlightAppToken = @"appToken";
 
-// NSString* const kFlurryAPIKey = @"";
 // Or comment out all references to Flurry if not using it.
+// NSString* const kFlurryAPIKey = @"flurryKey";
