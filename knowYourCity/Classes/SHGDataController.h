4d17d955262904bba99c5298b42f9fc15a976615
Matt Blair <elsewisemedia@gmail.com>
2013-10-14 10:56:49 -0700

    Added data controller singleton

---
+++ knowYourCity/Classes/SHGDataController.h
@@ -0,0 +1,13 @@
+//
+//  SHGDataController.h
+//  knowYourCity
+//
+//  Created by Matt Blair on 10/14/13.
+//  Copyright (c) 2013 Elsewise LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHGDataController : NSObject
+
+@end
---
+++ knowYourCity/Classes/SHGDataController.m
@@ -0,0 +1,13 @@
+//
+//  SHGDataController.m
+//  knowYourCity
+//
+//  Created by Matt Blair on 10/14/13.
+//  Copyright (c) 2013 Elsewise LLC. All rights reserved.
+//
+
+#import "SHGDataController.h"
+
+@implementation SHGDataController
+
+@end
