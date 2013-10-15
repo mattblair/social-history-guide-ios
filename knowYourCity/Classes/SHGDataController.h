af71c14503769b2ef9e505e15428f38dd236f691
Matt Blair <elsewisemedia@gmail.com>
2013-10-14 18:44:19 -0700

    Initial implementation of data controller

--- knowYourCity/Classes/SHGDataController.h
+++ knowYourCity/Classes/SHGDataController.h
@@ -8,6 +8,47 @@
 
 #import <Foundation/Foundation.h>
 
+// temporary -- will be 6
+#define PUBLISHED_WORKFLOW_STATE 2
+
+// Theme keys
+#define kThemeIDKey @"id"
+#define kThemeTitleKey @"title"
+#define kThemeSubtitleKey @"subtitle"
+#define kThemeSummaryKey @"summary"
+#define kThemeImageKey @"image_name"
+#define kThemeSlugKey @"slug"
+
+// also: latitude, longitude, zoom_level -- available through helper method?
+
+// Story keys
+#define kStoryIDKey @"id"
+#define kStoryTitleKey @"title"
+#define kStorySubtitleKey @"subtitle"
+#define kStorySummaryKey @"summary"
+#define kStoryImageKey @"image_name"
+#define kStorySlugKey @"slug"
+
+// Guest keys
+
+
+
 @interface SHGDataController : NSObject
+
+// returns dictionaries
+@property (strong, nonatomic) NSArray *publishedThemes;
+
++ (SHGDataController *)sharedInstance;
+
+
+// Themes
+
+// Stories
+
+- (NSArray *)storiesForThemeID:(NSUInteger)themeID;
+
+
+// Guests
+
 
 @end
--- knowYourCity/Classes/SHGDataController.m
+++ knowYourCity/Classes/SHGDataController.m
@@ -8,6 +8,95 @@
 
 #import "SHGDataController.h"
 
+#import <FMDB/FMDatabase.h>
+
+@interface SHGDataController ()
+
+@property (strong, nonatomic) FMDatabase *shgDatabase;
+
+@end
+
 @implementation SHGDataController
+
+#pragma mark - Singleton and Init Code
+
++ (SHGDataController *)sharedInstance {
+    
+    DEFINE_SHARED_INSTANCE_USING_BLOCK(^{
+        return [[self alloc] init];
+    });
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        
+        NSString *dbPath = [[NSBundle mainBundle] pathForResource:@"shg-web"
+                                                           ofType:@"sqlite"];
+        
+        self.shgDatabase = [FMDatabase databaseWithPath:dbPath];
+        
+        if (![self.shgDatabase open]) {
+            
+            DLog(@"Database failed");
+        }
+    }
+    return self;
+}
+
+- (void)dealloc {
+    
+    if (self.shgDatabase) {
+        
+        if ([self.shgDatabase hasOpenResultSets]) {
+            [self.shgDatabase closeOpenResultSets];
+        }
+        
+        [self.shgDatabase close];
+    }
+}
+
+#pragma mark - Themes
+
+- (NSArray *)publishedThemes {
+    
+    if (!_publishedThemes) {
+        
+        NSMutableArray *themes = [[NSMutableArray alloc] initWithCapacity:15];
+        
+        FMResultSet *s = [self.shgDatabase executeQueryWithFormat:@"SELECT * FROM themes where workflow_state_id = %d;", PUBLISHED_WORKFLOW_STATE];
+        
+        while ([s next]) {
+            
+            [themes addObject:[s resultDictionary]];
+            DLog(@"Theme title: %@", [s stringForColumn:kThemeTitleKey]);
+        }
+        
+        _publishedThemes = [NSArray arrayWithArray:themes];
+    }
+    
+    return _publishedThemes;
+}
+
+#pragma mark - Stories
+
+- (NSArray *)storiesForThemeID:(NSUInteger)themeID {
+    
+    NSMutableArray *stories = [[NSMutableArray alloc] initWithCapacity:10];
+    
+    FMResultSet *resultSet = [self.shgDatabase executeQueryWithFormat:@"SELECT * FROM stories where theme_id = %d and workflow_state_id = %d;", themeID, PUBLISHED_WORKFLOW_STATE];
+
+    while ([resultSet next]) {
+        
+        [stories addObject:[resultSet resultDictionary]];
+        
+    }
+    
+    return [NSArray arrayWithArray:stories];
+}
+
+#pragma mark - Guests
+
+
 
 @end
