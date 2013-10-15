44bc11130ee924e4553d1a96ac26a704c8ee0cf4
Matt Blair <elsewisemedia@gmail.com>
2013-10-15 11:26:29 -0700

    Added guest keys, refactored duck-typed keys

--- knowYourCity/Classes/SHGDataController.h
+++ knowYourCity/Classes/SHGDataController.h
@@ -6,32 +6,51 @@
 //  Copyright (c) 2013 Elsewise LLC. All rights reserved.
 //
 
+
+// At least in the first version:
+// Read from a cleaned up archive of the database from the Rails site.
+// In the future: could read from API, or fetched JSON, and persist to Core Data.
+// That might be overkill, since this is a relatively static, read-only dataset.
+
 #import <Foundation/Foundation.h>
 
 // temporary -- will be 6
 #define PUBLISHED_WORKFLOW_STATE 2
 
+// Cross-table keys
+
+#define kContentIDKey @"id"
+#define kContentTitleKey @"title"
+#define kContentSubtitleKey @"subtitle"
+#define kContentSlugKey @"slug"
+
+#define kContentLatitudeKey @"latitude"
+#define kContentLongitudeKey @"longitude"
+#define kContentZoomLevelKey @"zoom_level"
+
 // Theme keys
 #define kThemeIDKey @"id"
-#define kThemeTitleKey @"title"
-#define kThemeSubtitleKey @"subtitle"
 #define kThemeSummaryKey @"summary"
 #define kThemeImageKey @"image_name"
-#define kThemeSlugKey @"slug"
 
 // also: latitude, longitude, zoom_level -- available through helper method?
 
 // Story keys
 #define kStoryIDKey @"id"
-#define kStoryTitleKey @"title"
-#define kStorySubtitleKey @"subtitle"
 #define kStorySummaryKey @"summary"
 #define kStoryImageKey @"image_name"
-#define kStorySlugKey @"slug"
 
 // Guest keys
-
-
+#define kGuestIDKey @"id"
+#define kGuestNameKey @"name"
+#define kGuestTitleKey @"title"
+#define kGuestOrganizationKey @"organization"
+#define kGuestImageKey @"image_name"
+#define kGuestBiographyKey @"bio"
+#define kGuestQuoteKey @"quote"
+#define kGuestURLKey @"guest_url"
+#define kGuestURLTextKey @"guest_url_text"
+#define kGuestSpecialtyKey @"specialty"
 
 @interface SHGDataController : NSObject
 
@@ -42,6 +61,8 @@
 
 
 // Themes
+
+
 
 // Stories
 
