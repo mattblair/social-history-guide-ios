7c0f27d257270e6a8b268741c6a1f87353af7d48
Matt Blair <elsewisemedia@gmail.com>
2013-10-23 14:46:02 -0700

    Replaced caption label with photo metadata view

--- knowYourCity/Classes/StoryViewController.m
+++ knowYourCity/Classes/StoryViewController.m
@@ -15,11 +15,13 @@
 
 @property (nonatomic) CGFloat yForNextView;
 
+@property (nonatomic) BOOL metadataVisible;
+
 @property (strong, nonatomic) UIScrollView *scrollView;
 @property (strong, nonatomic) UIImageView *mainPhoto;
-@property (strong, nonatomic) UILabel *captionLabel;
 
-// image credit needed here
+@property (strong, nonatomic) UIButton *metadataButton;
+@property (strong, nonatomic) UIView *mediaMetadataView;
 
 @property (strong, nonatomic) UILabel *titleLabel;
 @property (strong, nonatomic) UILabel *subtitleLabel;
@@ -38,7 +40,8 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
+        _metadataVisible = NO;
     }
     return self;
 }
@@ -61,7 +64,7 @@
     
     // layout content -- mediaType will drive this
     
-    self.yForNextView = 0.0;
+    self.yForNextView = 10.0;
     
     KYCStoryMediaType mediaType = [[self.storyData objectForKey:kContentMediaTypeKey] unsignedIntegerValue];
     
@@ -69,42 +72,62 @@
             
         case KYCStoryMediaTypePhotoInterview: {
             
-            self.mainPhoto = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, self.yForNextView, MAIN_PHOTO_WIDTH, MAIN_PHOTO_HEIGHT)];
+            // test validity of imageName here, too?
             
             NSString *imageName = [self.storyData objectForKey:kStoryImageKey];
             
-            [self.mainPhoto setImageWithURL:[SHG_DATA urlForPhotoNamed:imageName]
-                           placeholderImage:[SHG_DATA photoPlaceholder]];
-            
-            [self.scrollView addSubview:self.mainPhoto];
-            
-            self.yForNextView = CGRectGetMaxY(self.mainPhoto.frame) + VERTICAL_SPACER_EXTRA;
-            
-            self.captionLabel = [[UILabel alloc] initWithFrame:CGRectMake(DEFAULT_LEFT_MARGIN, self.yForNextView, DEFAULT_CONTENT_WIDTH, 31.0)];
-            self.captionLabel.numberOfLines = 0;
-            self.captionLabel.lineBreakMode = NSLineBreakByWordWrapping;
-            self.captionLabel.text = [self.storyData objectForKey:kContentImageCaptionKey];
-            self.captionLabel.font = [UIFont fontWithName:kBodyFontName size:14.0];
-            
-            [self.captionLabel sizeToFit];
-            
-            [self.scrollView addSubview:self.captionLabel];
-            
-            self.yForNextView = CGRectGetMaxY(self.mainPhoto.frame) + VERTICAL_SPACER_EXTRA;
-            
-            // image credit here
+            if ([imageName length] > 0) {
+                
+                
+                self.mainPhoto = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, MAIN_PHOTO_WIDTH, MAIN_PHOTO_HEIGHT)];
+                
+                [self.mainPhoto setImageWithURL:[SHG_DATA urlForPhotoNamed:imageName]
+                               placeholderImage:[SHG_DATA photoPlaceholder]];
+                
+                self.mainPhoto.userInteractionEnabled = YES;
+                
+                UITapGestureRecognizer *photoTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                           action:@selector(toggleMetadataDisplay)];
+                [self.mainPhoto addGestureRecognizer:photoTap];
+                
+                [self.scrollView addSubview:self.mainPhoto];
+                
+                self.yForNextView = CGRectGetMaxY(self.mainPhoto.frame) + VERTICAL_SPACER_EXTRA;
+                
+                // setup caption trigger button ? Or gesture only?
+                
+                self.metadataButton = [UIButton buttonWithType:UIButtonTypeInfoDark];
+                
+                CGFloat mdButtonSize = 44.0; // probably depends on graphic
+                
+                self.metadataButton.frame = CGRectMake(MAIN_PHOTO_WIDTH - mdButtonSize, MAIN_PHOTO_HEIGHT - mdButtonSize,
+                                                       mdButtonSize, mdButtonSize);
+                
+                [self.metadataButton addTarget:self
+                                        action:@selector(toggleMetadataDisplay)
+                              forControlEvents:UIControlEventTouchUpInside];
+                
+                [self.scrollView addSubview:self.metadataButton];
+                
+                [self.scrollView addSubview:[self mediaMetadataView]];
+                
+            } else {
+                
+                DLog(@"Image name is missing.");
+            }
             
             break;
         }
             
-        case KYCStoryMediaTypeNoMedia:
+        case KYCStoryMediaTypeNoMedia: {
+            
             DLog(@"No media to display");
             break;
+        }
         
         case KYCStoryMediaTypeGeoJSONMap: {
             
             DLog(@"Would show points on the map...");
-            
             break;
         }
             
@@ -228,6 +251,97 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Subviews
+
+- (UIView *)mediaMetadataView {
+    
+    if (!_mediaMetadataView) {
+        
+        _mediaMetadataView = [[UIView alloc] initWithFrame:self.mainPhoto.frame];
+        
+        _mediaMetadataView.backgroundColor = [UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:0.85];
+        UIColor *captionTextColor = [UIColor kycDarkGray];
+        
+        CGFloat nextLabelY = 5.0;
+        
+        // setup fields
+        
+        UILabel *captionLabel = [[UILabel alloc] initWithFrame:CGRectMake(DEFAULT_LEFT_MARGIN, nextLabelY,
+                                                                          DEFAULT_CONTENT_WIDTH, 31.0)];
+        captionLabel.numberOfLines = 0;
+        captionLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        captionLabel.text = [self.storyData objectForKey:kContentImageCaptionKey];
+        captionLabel.font = [UIFont fontWithName:kBodyFontName size:14.0];
+        captionLabel.textColor = captionTextColor;
+        
+        [captionLabel sizeToFit];
+        
+        [_mediaMetadataView addSubview:captionLabel];
+        
+        nextLabelY = CGRectGetMaxY(captionLabel.frame) + 5.0;
+        
+        // add credit
+        
+        // see media_credit_markup method of stories_helper.rb for reference
+        
+        NSString *creditString = [self.storyData objectForKey:kContentImageCreditKey];
+        NSString *rawCopyright = [self.storyData objectForKey:kContentImageCopyrightNoticeKey];
+        
+        NSString *copyrightString = rawCopyright ? rawCopyright : NSLocalizedString(@"Used by Permission", @"Default copyright notice");
+        
+        // would metadata be shown in the footer of the map?
+        NSString *creditPrefix = NO ? @"Map Data" : @"Image";
+        
+        UILabel *creditLabel = [[UILabel alloc] initWithFrame:CGRectMake(DEFAULT_LEFT_MARGIN, nextLabelY,
+                                                                         DEFAULT_CONTENT_WIDTH, 31.0)];
+        creditLabel.textAlignment = NSTextAlignmentRight;
+        creditLabel.numberOfLines = 0;
+        creditLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        creditLabel.text = [NSString stringWithFormat:@"%@: %@ %@", creditPrefix, creditString, copyrightString];
+        creditLabel.font = [UIFont fontWithName:kBodyFontName size:12.0];
+        creditLabel.textColor = captionTextColor;
+        
+        [creditLabel sizeToFit];
+        
+        [_mediaMetadataView addSubview:creditLabel];
+        
+        nextLabelY = CGRectGetMaxY(creditLabel.frame) + 5.0;
+        
+        // add weblink? with just a tappable website text?
+        
+        // resize to bottom of image frame
+        
+        CGFloat metadataY = self.mainPhoto.frame.size.height - nextLabelY;
+        
+        _mediaMetadataView.frame = CGRectMake(0.0, metadataY, 320.0, nextLabelY);
+        _mediaMetadataView.alpha = _metadataVisible ? 1.0 : 0.0;
+        
+        UITapGestureRecognizer *mdTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                action:@selector(toggleMetadataDisplay)];
+        [_mediaMetadataView addGestureRecognizer:mdTap];
+    }
+    
+    return _mediaMetadataView;
+}
+
+#pragma mark - Responding to user gestures
+
+- (void)toggleMetadataDisplay {
+    
+    // based on alpha, or another state variable?
+    
+    if (self.metadataVisible) {
+        
+        self.metadataVisible = NO;
+    } else {
+        self.metadataVisible = YES;
+    }
+    
+    [UIView animateWithDuration:0.35
+                     animations:^{self.mediaMetadataView.alpha = self.metadataVisible ? 1.0 : 0.0;}
+                     completion:NULL];
 }
 
 @end
